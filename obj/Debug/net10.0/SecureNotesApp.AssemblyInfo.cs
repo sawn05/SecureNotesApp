94c6cf5038295d1b4fe30bf8caeb7e3d434dd558
Kien2412005 <kienhappy241@gmail.com>
2026-03-03 23:21:59 +0700

    complete UI - add site2.js full implementation

--- obj/Debug/net10.0/SecureNotesApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/SecureNotesApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecureNotesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90233455cd9d9c584b52bb7286cc3963d64ac54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6390053ac0d806cd0a54c8a7c02d95f6d8ffc8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecureNotesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecureNotesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
